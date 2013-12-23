620ee849267db5d8a8a25e3713446fb229b257fd
Jennifer Shelton <kstate.bioinformatics@gmail.com>
2013-12-23 12:47:50 -0600

    Update run_align.sh

--- run_align.sh
+++ run_align.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #
-#   USAGE: nohup bash run_align.sh &
+#   USAGE: sudo nohup bash run_align.sh &
 #
 #
 
@@ -10,19 +10,19 @@
 ######### align reads to tcas abyss assembly ################
 #############################################################
 
-bash /home/sheltonj/scripts/align_reads.sh /home/sheltonj/tca_ref_chrLG_all_to_Kmer_merges_81-scaffolds/reads/Trib_cast_R1_1000_good_good.fastq /home/sheltonj/tca_ref_chrLG_all_to_Kmer_merges_81-scaffolds/reads/Trib_cast_R2_1000_good_good.fastq /home/sheltonj/tca_ref_chrLG_all_to_Kmer_merges_81-scaffolds/Kmer_merges_81-scaffolds.fa
+bash /home/sheltonj/scripts/align_reads.sh /home/sheltonj/ubuntu/Trib_cast_R1_1000_good_good.fastq /home/sheltonj/ubuntu/Trib_cast_R2_1000_good_good.fastq /home/sheltonj/tca_ref_chrLG_all_to_Kmer_merges_81-scaffolds/Kmer_merges_81-scaffolds.fa
 
-bash /home/sheltonj/scripts/align_reads.sh /home/sheltonj/tca_ref_chrLG_all_to_Kmer_merges_81-scaffolds/reads/Trib_cast_R1_2000_good_good.fastq /home/sheltonj/tca_ref_chrLG_all_to_Kmer_merges_81-scaffolds/reads/Trib_cast_R2_2000_good_good.fastq /home/sheltonj/tca_ref_chrLG_all_to_Kmer_merges_81-scaffolds/Kmer_merges_81-scaffolds.fa
+bash /home/sheltonj/scripts/align_reads.sh /home/sheltonj/ubuntu/Trib_cast_R1_2000_good_good.fastq /home/sheltonj/ubuntu/Trib_cast_R2_2000_good_good.fastq /home/sheltonj/tca_ref_chrLG_all_to_Kmer_merges_81-scaffolds/Kmer_merges_81-scaffolds.fa
 
-bash /home/sheltonj/scripts/align_reads.sh /home/sheltonj/tca_ref_chrLG_all_to_Kmer_merges_81-scaffolds/reads/Trib_cast_R1_3000_good_good.fastq /home/sheltonj/tca_ref_chrLG_all_to_Kmer_merges_81-scaffolds/reads/Trib_cast_R2_3000_good_good.fastq /home/sheltonj/tca_ref_chrLG_all_to_Kmer_merges_81-scaffolds/Kmer_merges_81-scaffolds.fa 
+bash /home/sheltonj/scripts/align_reads.sh /home/sheltonj/ubuntu/Trib_cast_R1_3000_good_good.fastq /home/sheltonj/ubuntu/Trib_cast_R2_3000_good_good.fastq /home/sheltonj/tca_ref_chrLG_all_to_Kmer_merges_81-scaffolds/Kmer_merges_81-scaffolds.fa 
 
-bash /home/sheltonj/scripts/align_reads.sh /home/sheltonj/tca_ref_chrLG_all_to_Kmer_merges_81-scaffolds/reads/Tribolium3kb_1_prinseq_good_P7uO.fastq /home/sheltonj/tca_ref_chrLG_all_to_Kmer_merges_81-scaffolds/reads/Tribolium3kb_2_prinseq_good_36G8.fastq /home/sheltonj/tca_ref_chrLG_all_to_Kmer_merges_81-scaffolds/Kmer_merges_81-scaffolds.fa 
+bash /home/sheltonj/scripts/align_reads.sh /home/sheltonj/ubuntu/Tribolium3kb_1_prinseq_good_P7uO.fastq /home/sheltonj/ubuntu/Tribolium3kb_2_prinseq_good_36G8.fastq /home/sheltonj/tca_ref_chrLG_all_to_Kmer_merges_81-scaffolds/Kmer_merges_81-scaffolds.fa 
 
-bash /home/sheltonj/scripts/align_reads.sh /home/sheltonj/tca_ref_chrLG_all_to_Kmer_merges_81-scaffolds/reads/Trib_cast_R1_3500_good_good.fastq /home/sheltonj/tca_ref_chrLG_all_to_Kmer_merges_81-scaffolds/reads/Trib_cast_R2_3500_good_good.fastq /home/sheltonj/tca_ref_chrLG_all_to_Kmer_merges_81-scaffolds/Kmer_merges_81-scaffolds.fa
+bash /home/sheltonj/scripts/align_reads.sh /home/sheltonj/ubuntu/Trib_cast_R1_3500_good_good.fastq /home/sheltonj/ubuntu/Trib_cast_R2_3500_good_good.fastq /home/sheltonj/tca_ref_chrLG_all_to_Kmer_merges_81-scaffolds/Kmer_merges_81-scaffolds.fa
 
-bash /home/sheltonj/scripts/align_reads.sh /home/sheltonj/tca_ref_chrLG_all_to_Kmer_merges_81-scaffolds/reads/Trib_cast_R1_36000_good_good.fastq /home/sheltonj/tca_ref_chrLG_all_to_Kmer_merges_81-scaffolds/reads/Trib_cast_R2_36000_good_good.fastq /home/sheltonj/tca_ref_chrLG_all_to_Kmer_merges_81-scaffolds/Kmer_merges_81-scaffolds.fa
+bash /home/sheltonj/scripts/align_reads.sh /home/sheltonj/ubuntu/Trib_cast_R1_36000_good_good.fastq /home/sheltonj/ubuntu/Trib_cast_R2_36000_good_good.fastq /home/sheltonj/tca_ref_chrLG_all_to_Kmer_merges_81-scaffolds/Kmer_merges_81-scaffolds.fa
 
-bash /home/sheltonj/scripts/align_reads.sh /home/sheltonj/tca_ref_chrLG_all_to_Kmer_merges_81-scaffolds/reads/Tribolium20kb_1_prinseq_good_xq20.fastq /home/sheltonj/tca_ref_chrLG_all_to_Kmer_merges_81-scaffolds/reads/Tribolium20kb_2_prinseq_good_10IC.fastq /home/sheltonj/tca_ref_chrLG_all_to_Kmer_merges_81-scaffolds/Kmer_merges_81-scaffolds.fa 
+bash /home/sheltonj/scripts/align_reads.sh /home/sheltonj/ubuntu/Tribolium20kb_1_prinseq_good_xq20.fastq /home/sheltonj/ubuntu/Tribolium20kb_2_prinseq_good_10IC.fastq /home/sheltonj/tca_ref_chrLG_all_to_Kmer_merges_81-scaffolds/Kmer_merges_81-scaffolds.fa 
 
-bash /home/sheltonj/scripts/align_reads.sh /home/sheltonj/tca_ref_chrLG_all_to_Kmer_merges_81-scaffolds/reads/Tribolium8kb_1_prinseq_good_ffRe.fastq /home/sheltonj/tca_ref_chrLG_all_to_Kmer_merges_81-scaffolds/reads/Tribolium8kb_2_prinseq_good_RaKN.fastq /home/sheltonj/tca_ref_chrLG_all_to_Kmer_merges_81-scaffolds/Kmer_merges_81-scaffolds.fa
+bash /home/sheltonj/scripts/align_reads.sh /home/sheltonj/ubuntu/Tribolium8kb_1_prinseq_good_ffRe.fastq /home/sheltonj/ubuntu/Tribolium8kb_2_prinseq_good_RaKN.fastq /home/sheltonj/tca_ref_chrLG_all_to_Kmer_merges_81-scaffolds/Kmer_merges_81-scaffolds.fa
 
