2d6bcc6058adbd9ee8be71a0e5f29201dd733b57
Jennifer Shelton <kstate.bioinformatics@gmail.com>
2014-01-02 10:24:49 -0600

    Update find_super-scaffolds.pl

--- find_super-scaffolds.pl
+++ find_super-scaffolds.pl
@@ -3,6 +3,9 @@
 #   
 #	USAGE: perl find_super-scaffolds.pl
 #	perl find_super-scaffolds.pl test.pctgs
+#   Script lists changes gam-ngs has made to a genome assembly and reverts 
+#   scaffold headers to original headers if they have not been altered by 
+#   gam-ngs.
 #
 #  Created by jennifer shelton
 #
@@ -17,55 +20,129 @@ use warnings;
 my $pctgs = $ARGV[0];
 open (SUMMARY, '<', $pctgs) or die "can't open $pctgs!\n";
 $pctgs =~ /(.*).pctgs/;
-print "$1\n";
 open (OUT, '>', "$1.txt") or die "can't open $1.txt\n";
-my ($PairedContig,$Assembly,$ContigID,$Begin,$End,$Reversed);
+my ($PairedContig,$Size,$Assembly,$ContigID,$Begin,$End,$Reversed);
 my $line=0;
-my %super_scaff;
+my $firstBegin;
+my (%keepnames,%losenames);
 my @columns;
 #Name	Size	Assembly	ContigID	Begin	End	Reversed
 while (<SUMMARY>)
 {
 	unless (/^#/)
 	{
+	    chomp;
+		@columns=split/\t/;
 		unless ($line==0)
 		{
-			chomp;
-			@columns=split/\t/;
 			
 			###############################################
 			########## check for new contig on paired #####
 			###############################################
  			if (($PairedContig eq $columns[0])&&($ContigID ne $columns[3]))
  			{
-				 push @{ $super_scaff{$columns[0]} },$columns[3];
-				 print "$ContigID ne $columns[3]\n";
+				 $losenames{$columns[0]}=$columns[3];
+				 print "\nSUPER-SCAFFOLD: ContigID to $columns[3]\n";
  			}
 			###############################################
 			########## check for skipped contig       #####
 			###############################################
 			elsif (($PairedContig eq $columns[0])&&(($End + 1 ) != $columns[4])&& ($Reversed eq 'F')&&($columns[6] eq 'F'))
  			{
- 				print OUT "Prior: $PairedContig,$Assembly,$ContigID,$Begin,$End,$Reversed\n";
+ 			    $losenames{$columns[0]}=$columns[3];
+ 				print OUT "\nBROKEN SCAFFOLD: Prior: $PairedContig,$Assembly,$ContigID,$Begin,$End,$Reversed\n";
  				print OUT "Next: $columns[0],$columns[2],$columns[3],$columns[4],$columns[5],$columns[6]\n";
  			}
-		($PairedContig,$Assembly,$ContigID,$Begin,$End,$Reversed)=($columns[0],$columns[2],$columns[3],$columns[4],$columns[5],$columns[6]);
-		}
+ 			###############################################
+            ########## check for reversed sections    #####
+            ###############################################
+            elsif ($columns[6] eq 'R')
+            {
+                print "\nPARTIALLY REVERSED SCAFFOLDED: $columns[3]\n";
+                $losenames{$columns[0]}=$columns[3];
+            }
+            ################################################################
+            ##########    check that the contig is still full length   #####
+            ################################################################
+            if ($PairedContig ne $columns[0])
+            {
+                if (($firstBegin!=0)||(($Size-1) != $End))
+                {
+                    $losenames{$PairedContig}=$ContigID;
+                }
+                elsif (!$losenames{$PairedContig})
+                {
+                    $keepnames{$PairedContig}=$ContigID;
+                }
+
+            }
+            ###############################################
+            ##########    redefine the last contig    #####
+            ###############################################
+            ($PairedContig,$Size,$Assembly,$ContigID,$Begin,$End,$Reversed)=($columns[0],$columns[1],$columns[2],$columns[3],$columns[4],$columns[5],$columns[6]);
+        }
 		###############################################
 		########## initialize first row     ###########
 		###############################################
 		###############################################
 		## initialize the first entry for a paired ####
 		###############################################
-		elsif (($line==0)||($PairedContig ne $columns[0]))
+		if ($line==0)
 		{
-			chomp;
-			@columns=split/\t/;
-		($PairedContig,$Assembly,$ContigID,$Begin,$End,$Reversed)=($columns[0],$columns[2],$columns[3],$columns[4],$columns[5],$columns[6]);
-			push @{ $super_scaff{$columns[0]} },$columns[3];
+			
+            ($PairedContig,$Size,$Assembly,$ContigID,$Begin,$End,$Reversed)=($columns[0],$columns[1],$columns[2],$columns[3],$columns[4],$columns[5],$columns[6]);
+		    $firstBegin=$Begin;
 			++$line;
 			
 		}
+		if (eof)
+		{
+            if (($firstBegin!=0)||(($Size-1) != $End))
+             {
+                 $losenames{$columns[0]}=$columns[3];
+             }
+             elsif (!$losenames{$columns[0]}) 
+            {
+                $keepnames{$columns[0]}=$columns[3];
+            }
+        }
 	}
 }
+print "\n";
+##################################################################################
+##############          Revert unaltered fasta headers          ##################
+##################################################################################
+my $fasta=$ARGV[1];
+open FASTA,'<', $fasta or die "Can't open $fasta\n!";
+$fasta =~ /(.*)(\.fa.*)/;
+
+open NEWFASTA,'>', "$1_revert.fasta" or die "Can't open $fasta\n!";
+$/ = ">"; ### each input will equal an entire fasta record
+my $seq_count=0;
+while (<FASTA>)
+{
+    unless (${seq_count} == 0) ## skip first record (it is a blank line)
+    {
+        s/>//g;
+        my ($header, @seqLines) = split /\n/;
+        if ($keepnames{$header})
+        {
+            print NEWFASTA ">$keepnames{$header}\n";
+            print NEWFASTA join ("\n", @seqLines);
+            print NEWFASTA "\n";
+        }
+        else
+        {
+            print NEWFASTA;
+        }
+    }
+    else {$seq_count=1};
+}
+
+
+for my $con (keys %losenames)
+{
+    print "$con has been altered\n";
+}
+
 		
