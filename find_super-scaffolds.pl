19c264e7675fa9148d0b8be5f9c9d6d704681f2a
Jennifer Shelton <kstate.bioinformatics@gmail.com>
2014-07-18 12:51:09 -0500

    Update find_super-scaffolds.pl

--- find_super-scaffolds.pl
+++ find_super-scaffolds.pl
@@ -1,13 +1,13 @@
 #!/bin/perl
 ##################################################################################
-#   
+#	
 #	USAGE: perl find_super-scaffolds.pl PCTG_FILE
 #	perl find_super-scaffolds.pl test.pctgs
-#   Script lists changes gam-ngs has made to a genome assembly and reverts 
-#   scaffold headers to original headers if they have not been altered by 
-#   gam-ngs.
+#	Script lists changes gam-ngs has made to a genome assembly and reverts 
+#	scaffold headers to original headers if they have not been altered by 
+#	gam-ngs.
 #
-#  Created by jennifer shelton
+#	Created by jennifer shelton
 #
 ##################################################################################
 use strict;
@@ -17,7 +17,7 @@ $Text::Wrap::columns = 61; #includes terminal \n
 # use List::Util qw(max);
 # use List::Util qw(sum);
 ##################################################################################
-##############                      notes                       ##################
+############## 					  notes 					   ##################
 ##################################################################################
 my $pctgs = $ARGV[0];
 open (SUMMARY, '<', $pctgs) or die "can't open $pctgs!\n";
@@ -32,7 +32,7 @@ while (<SUMMARY>)
 {
 	unless (/^#/)
 	{
-	    chomp;
+		chomp;
 		@columns=split/\t/;
 		unless ($line==0)
 		{
@@ -50,38 +50,38 @@ while (<SUMMARY>)
 			###############################################
 			elsif (($PairedContig eq $columns[0])&&(($End + 1 ) != $columns[4])&& ($Reversed eq 'F')&&($columns[6] eq 'F'))
  			{
- 			    $losenames{$columns[0]}=$columns[3];
+ 				$losenames{$columns[0]}=$columns[3];
  				print "\nBROKEN SCAFFOLD: Prior: $PairedContig,$Assembly,$ContigID,$Begin,$End,$Reversed\n";
  				print "Next: $columns[0],$columns[2],$columns[3],$columns[4],$columns[5],$columns[6]\n";
  			}
  			###############################################
-            ########## check for reversed sections    #####
-            ###############################################
-            elsif ($columns[6] eq 'R')
-            {
-                print "\nPARTIALLY REVERSED SCAFFOLDED: $columns[3]\n";
-                $losenames{$columns[0]}=$columns[3];
-            }
-            ################################################################
-            ##########    check that the contig is still full length   #####
-            ################################################################
-            if ($PairedContig ne $columns[0])
-            {
-                if (($firstBegin!=0)||(($Size-1) != $End))
-                {
-                    $losenames{$PairedContig}=$ContigID;
-                }
-                elsif (!$losenames{$PairedContig})
-                {
-                    $keepnames{$PairedContig}=$ContigID;
-                }
+ 			########## check for reversed sections    #####
+ 			###############################################
+ 			elsif ($columns[6] eq 'R')
+ 			{
+ 				print "\nPARTIALLY REVERSED SCAFFOLDED: $columns[3]\n";
+ 				$losenames{$columns[0]}=$columns[3];
+ 			}
+ 			################################################################
+ 			##########    check that the contig is still full length   #####
+ 			################################################################
+ 			if ($PairedContig ne $columns[0])
+ 			{
+ 				if (($firstBegin!=0)||(($Size-1) != $End))
+ 				{
+ 					$losenames{$PairedContig}=$ContigID;
+ 				}
+ 				elsif (!$losenames{$PairedContig})
+ 				{
+ 					$keepnames{$PairedContig}=$ContigID;
+ 				}
 
-            }
-            ###############################################
-            ##########    redefine the last contig    #####
-            ###############################################
-            ($PairedContig,$Size,$Assembly,$ContigID,$Begin,$End,$Reversed)=($columns[0],$columns[1],$columns[2],$columns[3],$columns[4],$columns[5],$columns[6]);
-        }
+ 			}
+ 			###############################################
+ 			##########    redefine the last contig    #####
+ 			###############################################
+ 			($PairedContig,$Size,$Assembly,$ContigID,$Begin,$End,$Reversed)=($columns[0],$columns[1],$columns[2],$columns[3],$columns[4],$columns[5],$columns[6]);
+		}
 		###############################################
 		########## initialize first row     ###########
 		###############################################
@@ -91,27 +91,27 @@ while (<SUMMARY>)
 		if ($line==0)
 		{
 			
-            ($PairedContig,$Size,$Assembly,$ContigID,$Begin,$End,$Reversed)=($columns[0],$columns[1],$columns[2],$columns[3],$columns[4],$columns[5],$columns[6]);
-		    $firstBegin=$Begin;
+ 			($PairedContig,$Size,$Assembly,$ContigID,$Begin,$End,$Reversed)=($columns[0],$columns[1],$columns[2],$columns[3],$columns[4],$columns[5],$columns[6]);
+			$firstBegin=$Begin;
 			++$line;
 			
 		}
 		if (eof)
 		{
-            if (($firstBegin!=0)||(($Size-1) != $End))
-             {
-                 $losenames{$columns[0]}=$columns[3];
-             }
-             elsif (!$losenames{$columns[0]}) 
-            {
-                $keepnames{$columns[0]}=$columns[3];
-            }
-        }
+ 			if (($firstBegin!=0)||(($Size-1) != $End))
+ 			 {
+ 				$losenames{$columns[0]}=$columns[3];
+ 			 }
+ 			 elsif (!$losenames{$columns[0]}) 
+ 			{
+ 				$keepnames{$columns[0]}=$columns[3];
+ 			}
+		}
 	}
 }
 print "\n";
 ##################################################################################
-##############          Revert unaltered fasta headers          ##################
+##############		  Revert unaltered fasta headers		  ##################
 ##################################################################################
 my $fasta=$ARGV[1];
 open FASTA,'<', $fasta or die "Can't open $fasta\n!";
@@ -121,32 +121,32 @@ $/ = ">"; ### each input will equal an entire fasta record
 my $seq_count=0;
 while (<FASTA>)
 {
-    unless (${seq_count} == 0) ## skip first record (it is a blank line)
-    {
-        s/>//g;
-        my ($header, @seqLines) = split /\n/;
-        if ($keepnames{$header})
-        {
-            print NEWFASTA ">$keepnames{$header}\n";
-            @seqLines=join ('', @seqLines);
-            print NEWFASTA wrap('','',@seqLines);
-            print NEWFASTA "\n";
-        }
-        else
-        {
-            print NEWFASTA ">$header\n";
-            @seqLines=join ('', @seqLines);
-            print NEWFASTA wrap('','',@seqLines);
-            print NEWFASTA "\n";
-        }
-    }
-    else {$seq_count=1};
+	unless (${seq_count} == 0) ## skip first record (it is a blank line)
+	{
+		s/>//g;
+		my ($header, @seqLines) = split /\n/;
+		if ($keepnames{$header})
+		{
+ 			print NEWFASTA ">$keepnames{$header}\n";
+ 			@seqLines=join ('', @seqLines);
+ 			print NEWFASTA wrap('','',@seqLines);
+ 			print NEWFASTA "\n";
+		}
+		else
+		{
+ 			print NEWFASTA ">$header\n";
+ 			@seqLines=join ('', @seqLines);
+ 			print NEWFASTA wrap('','',@seqLines);
+ 			print NEWFASTA "\n";
+		}
+	}
+	else {$seq_count=1};
 }
 
 
 for my $con (keys %losenames)
 {
-    print "$con has been altered\n";
+	print "$con has been altered\n";
 }
 
 		
